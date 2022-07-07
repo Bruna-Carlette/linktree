e145472e7e71fff15461f3feb0e51b0a5ccd283f
Bruna Carlette <83823739+Bruna-Carlette@users.noreply.github.com>
2022-07-06 22:55:25 -0300

    Add files via upload
    Uso de changeTheme em JS.
    Uso de seletor genérico, hover e root no CSS.

---
+++ menu.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("menu").style.display = "block";
+
+}
+function closeNav() {
+    document.getElementById("menu").style.display = "none";
+}
+
+function changeTheme(theme) {
+    document.getElementById("body").classList = theme;
+}
